055965e8fffdefff2fa45e758f0688b1e59b1837
Garry Gonzales <garry.gonzales@aptiture.com>
2018-01-14 11:25:07 +0800

    MCP-49: Created initial screen for forgot password

--- mobile-ionic-src/src/app/app.module.ts
+++ mobile-ionic-src/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/index';
+import { ForgotPasswordPage } from '../pages/login/forgot-password/index';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    ForgotPasswordPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    LoginPage
+    LoginPage,
+    ForgotPasswordPage
   ],
   providers: [
     StatusBar,
--- mobile-ionic-src/src/pages/login/index.ts
+++ mobile-ionic-src/src/pages/login/index.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ForgotPasswordPage } from './forgot-password/index';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'index.html'
@@ -9,6 +11,10 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onClickForgotPassword() {
+    this.navCtrl.push(ForgotPasswordPage);
   }
 
 }
