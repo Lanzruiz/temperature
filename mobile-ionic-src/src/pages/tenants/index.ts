3558823f6b393ed5b49d8b6d6158a088f1ffe6e0
Garry Gonzales <garry.gonzales@aptiture.com>
2018-02-26 05:10:01 +0800

    Updated 'Tenants List' screen as per UI design

--- mobile-ionic-src/src/pages/tenants/index.ts
+++ mobile-ionic-src/src/pages/tenants/index.ts
@@ -18,56 +18,56 @@ export class TenantsPage {
       email: 'info@warwick.com',
       version: '3.0.1555',
       size: '32mb',
+      color: 'pink'
+    },
+    {
+      name: 'NHS Trust',
+      email: 'info@nhstrust.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'darkblue'
+    },
+    {
+      name: 'Monika',
+      email: 'info@worcessterollege.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'darkblue'
+    },
+    {
+      name: 'Worcesster College',
+      email: 'info@worcessterollege.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'pink'
+    },
+    {
+      name: 'NHS Trust',
+      email: 'info@nhstrust.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'pink'
+    },
+    {
+      name: 'Warwick',
+      email: 'info@warwick.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'pink'
+    },
+    {
+      name: 'NHS Trust',
+      email: 'info@nhstrust.com',
+      version: '3.0.1555',
+      size: '32mb',
+      color: 'darkblue'
+    },
+    {
+      name: 'Monika',
+      email: 'info@monika.com',
+      version: '3.0.1555',
+      size: '500mb',
       color: 'red'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'yellow'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'green'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'green'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'red'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'yellow'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'red'
-    },
-    {
-      name: 'Worcesster College',
-      email: 'info@worcessterollege.com',
-      version: '3.0.1555',
-      size: '32mb',
-      color: 'blue'
     }
   ];
 
